e789e5d255eaf5a7ff30bfd1f29b7618421c334e
Enzo Bonfante <enzobonfx@gmail.com>
2024-03-07 22:21:46 -0300

    feat: rota p/ buscar endereço por cep

--- API/src/endereco/endereco.controller.ts
+++ API/src/endereco/endereco.controller.ts
@@ -1,45 +1,12 @@
-import {
-	Controller,
-	Get,
-	Post,
-	Body,
-	Patch,
-	Param,
-	Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { EnderecoService } from './endereco.service';
-import { CreateEnderecoDto } from './dto/create-endereco.dto';
-import { UpdateEnderecoDto } from './dto/update-endereco.dto';
 
-@Controller('endereco')
+@Controller('enderecos')
 export class EnderecoController {
 	constructor(private readonly enderecoService: EnderecoService) {}
 
-	@Post()
-	create(@Body() createEnderecoDto: CreateEnderecoDto) {
-		return this.enderecoService.create(createEnderecoDto);
-	}
-
-	@Get()
-	findAll() {
-		return this.enderecoService.findAll();
-	}
-
-	@Get(':id')
-	findOne(@Param('id') id: string) {
-		return this.enderecoService.findOne(+id);
-	}
-
-	@Patch(':id')
-	update(
-		@Param('id') id: string,
-		@Body() updateEnderecoDto: UpdateEnderecoDto,
-	) {
-		return this.enderecoService.update(+id, updateEnderecoDto);
-	}
-
-	@Delete(':id')
-	remove(@Param('id') id: string) {
-		return this.enderecoService.remove(+id);
+	@Get('cep/:cep')
+	findByCep(@Param('cep') cep: string) {
+		return this.enderecoService.findByCep(cep);
 	}
 }
---
+++ API/src/utils/isCepValido.ts
@@ -0,0 +1,4 @@
+export const isCepValido = (cep: string) => {
+	const cepRegex = /^[0-9]{5}-?[0-9]{3}$/;
+	return cepRegex.test(cep);
+};
