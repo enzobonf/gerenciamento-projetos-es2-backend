cde5cc1310a4597b1cfbb32641989d162b544f00
Alexandre Peiter Ferraz <alexandrepeiterferraz@gmail.com>
2024-03-08 20:58:18 -0300

    feature: CRUD cliente

---
+++ API/src/cliente/dto/create-cliente.dto.ts
@@ -0,0 +1,30 @@
+import { Type } from 'class-transformer';
+import {
+	IsNotEmpty,
+	IsNumber,
+	IsString
+} from 'class-validator';
+
+export class EnderecoClienteDto {
+	@IsString()
+	@IsNotEmpty()
+	cep: string;
+
+	@IsString()
+	@IsNotEmpty()
+	logradouro: string;
+
+	@IsString()
+	@IsNotEmpty()
+	bairro: string;
+
+	@IsNumber()
+	@IsNotEmpty()
+	id_cidade: number;
+
+	@IsString()
+	@IsNotEmpty()
+	sigla_uf: string;
+}
+
+export class CreateClienteDto {}
