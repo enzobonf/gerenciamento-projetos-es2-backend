55e0341f744380f0217c3e5af715885a7e9fa6d6
Alexandre Peiter Ferraz <alexandrepeiterferraz@gmail.com>
2024-03-08 21:13:01 -0300

    feature: CRUD especialidade

---
+++ API/src/especialidade/especialidade.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateEspecialidadeDto } from './dto/create-especialidade.dto';
+import { UpdateEspecialidadeDto } from './dto/update-especialidade.dto';
+import { PrismaService } from 'nestjs-prisma';
+
+@Injectable()
+export class EspecialidadeService {
+  constructor(private readonly prismaService: PrismaService) {}
+  /*
+  create(createEspecialidadeDto: CreateEspecialidadeDto) {
+    return 'This action adds a new especialidade';
+  }*/
+
+  findAll() {
+    return this.prismaService.especialidade.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prismaService.especialidade.findUnique({
+      where:{id}
+    });
+  }
+/*
+  update(id: number, updateEspecialidadeDto: UpdateEspecialidadeDto) {
+    return `This action updates a #${id} especialidade`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} especialidade`;
+  }*/
+}
