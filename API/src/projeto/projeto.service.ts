0cc272d2402b06d485c5d18908183958d9225604
Alexandre Peiter Ferraz <alexandrepeiterferraz@gmail.com>
2024-03-08 21:55:11 -0300

    feature: CRUD projeto

--- API/src/projeto/projeto.service.ts
+++ API/src/projeto/projeto.service.ts
@@ -1,26 +1,63 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProjetoDto } from './dto/create-projeto.dto';
 import { UpdateProjetoDto } from './dto/update-projeto.dto';
-
+import { PrismaService } from 'nestjs-prisma';
+import { Prisma } from '@prisma/client';
 @Injectable()
 export class ProjetoService {
+  constructor(private readonly prismaService: PrismaService) {}
+
   create(createProjetoDto: CreateProjetoDto) {
-    return 'This action adds a new projeto';
+    return  this.prismaService.projeto.create({
+			data: createProjetoDto,
+		});;
   }
 
-  findAll() {
-    return `This action returns all projeto`;
+  private formatProjeto(projeto: any) {
+		const { cliente } = projeto;
+    projeto.cliente = {
+      id: cliente.id_cliente,
+      nome: cliente.nome,
+      sobrenome: cliente.sobrenome,
+
+    }
+		return projeto;
+	}
+
+  private default_include: Prisma.projetoInclude = {
+    cliente:true,
+    time:true,
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} projeto`;
+  async findAll() {
+    const projetos: any[] =
+			await this.prismaService.projeto.findMany({
+      include:this.default_include,
+    });
+
+    projetos.forEach((x) => this.formatProjeto(x));
+		return { projetos };
+  }
+
+  async findOne(id: number) {
+    const projeto:any =await  this.prismaService.projeto.findUnique({
+      include:this.default_include,
+      where:{id},
+    });
+
+    return this.formatProjeto(projeto);
   }
 
   update(id: number, updateProjetoDto: UpdateProjetoDto) {
-    return `This action updates a #${id} projeto`;
+    return this.prismaService.projeto.update({
+			where: { id },
+			data: updateProjetoDto,
+		});
   }
 
   remove(id: number) {
-    return `This action removes a #${id} projeto`;
+    return this.prismaService.projeto.delete({
+			where: { id },
+		});
   }
 }
