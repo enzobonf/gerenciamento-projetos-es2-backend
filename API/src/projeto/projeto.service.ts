f645a971f0caac69f31cc11fa5677dcde4c28b55
Enzo Bonfante <enzobonfx@gmail.com>
2024-03-10 22:48:31 -0300

    fix: retorno do projeto no create/update

--- API/src/projeto/projeto.service.ts
+++ API/src/projeto/projeto.service.ts
@@ -8,30 +8,36 @@ import formatMoney from 'src/utils/formatMoney';
 export class ProjetoService {
 	constructor(private readonly prismaService: PrismaService) {}
 
-	create(createProjetoDto: CreateProjetoDto) {
+	private default_include: Prisma.projetoInclude = {
+		cliente: true,
+		time: true,
+	};
+
+	async create(createProjetoDto: CreateProjetoDto) {
 		createProjetoDto.data_fim = new Date(createProjetoDto.data_fim);
 		createProjetoDto.data_inicio = new Date(createProjetoDto.data_inicio);
 
-		return this.prismaService.projeto.create({
+		const projeto = await this.prismaService.projeto.create({
 			data: createProjetoDto,
+			include: this.default_include,
 		});
+
+		return this.formatProjeto(projeto);
 	}
 
 	private formatProjeto(projeto: any) {
 		const { cliente } = projeto;
 		projeto.cliente = {
-			id: cliente.id_cliente,
+			id: cliente.id,
 			nome: cliente.nome,
 			sobrenome: cliente.sobrenome,
+			email: cliente.email,
+			ddi_telefone: cliente.ddi_telefone,
+			num_telefone: cliente.num_telefone,
 		};
 		projeto.valor_str = formatMoney(projeto.valor);
 		return projeto;
 	}
-
-	private default_include: Prisma.projetoInclude = {
-		cliente: true,
-		time: true,
-	};
 
 	async findAll() {
 		const projetos: any[] = await this.prismaService.projeto.findMany({
@@ -51,11 +57,14 @@ export class ProjetoService {
 		return this.formatProjeto(projeto);
 	}
 
-	update(id: number, updateProjetoDto: UpdateProjetoDto) {
-		return this.prismaService.projeto.update({
+	async update(id: number, updateProjetoDto: UpdateProjetoDto) {
+		const projeto = await this.prismaService.projeto.update({
 			where: { id },
 			data: updateProjetoDto,
+			include: this.default_include,
 		});
+
+		return this.formatProjeto(projeto);
 	}
 
 	remove(id: number) {
